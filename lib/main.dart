c172f73ba643306a64d6cf5f955e02a5e02ae15f
shallagehsaeed9 <shallagehsaeed9@gmail.com>
2026-02-05 07:18:44 +0330

    Refactor WebView setup in main.dart
    Updated WebView initialization and removed platform-specific controller setup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 🔥 Full immersive mode (hide status + navigation bars)
   await SystemChrome.setEnabledSystemUIMode(
     SystemUiMode.immersiveSticky,
   );
@@ -40,31 +36,18 @@ class _WebViewPageState extends State<WebViewPage>
 
   late final WebViewController controller;
 
-@override
-void initState() {
-  super.initState();
-  WidgetsBinding.instance.addObserver(this);
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
 
-  final PlatformWebViewControllerCreationParams params =
-      const PlatformWebViewControllerCreationParams();
-
-  controller = WebViewController.fromPlatformCreationParams(params)
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(Uri.parse("http://54.38.152.132:5000/"));
-
-  // ✅ فعال‌سازی DOM Storage برای Android
-  if (controller.platform is AndroidWebViewController) {
-    final androidController =
-        controller.platform as AndroidWebViewController;
-
-    androidController.setJavaScriptEnabled(true);
-    androidController.setDomStorageEnabled(true);
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse("http://54.38.152.132:5000/"),
+      );
   }
-}
 
-
-
-  // 🔒 re-hide system bars after resume
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
@@ -81,5 +64,3 @@ void initState() {
     );
   }
 }
-
-
